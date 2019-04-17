8af9e2b3e09d16e99ff271e8f13aaf91dfe064f8
Pedro Cunial <pedrocc4@al.insper.edu.br>
2019-04-17 09:21:34 -0300

    added examplePedro.py
    parabolic SAR example

---
+++ examplePedro.py
@@ -0,0 +1,54 @@
+from backtesting import Strategy, Order, Event, evaluateHist
+
+
+class SAR(Strategy):
+
+    def __init__(self):
+        self.sar = []
+        self.highs = []
+        self.lows = []
+        self.accel_min = 0.02
+        self.accel_max = 0.2
+        self.accel = self.accel_min
+        self.crescent = True
+        self.buying = 0
+
+    def push(self, event):
+        high = event.price[1]
+        low = event.price[2]
+        price = event.price[3]
+        orders = []
+        if self.sar:
+            price = event.price[3]
+            sar_prev = self.sar[-1]
+            if self.crescent:
+                sar_predict = sar_prev + self.accel * (self.highs[-1] -
+                                                       sar_prev)
+                if sar_predict > price:
+                    self.crescent = False
+                    if self.buying == 1:
+                        orders += [Order(-1, 0), Order(-1, 0)]
+                        self.buying = -1
+                    elif self.buying == 0:
+                        orders.append(Order(-1, 0))
+                        self.buying = -1
+            else:
+                sar_predict = sar_prev + self.accel * (self.lows[-1] -
+                                                       sar_prev)
+                if sar_predict < price:
+                    self.crescent = True
+                    if self.buying == -1:
+                        orders += [Order(1, 0), Order(1, 0)]
+                        self.buying = 1
+                    elif self.buying == 0:
+                        orders.append(Order(1, 0))
+                        self.buying = 1
+        else:
+            sar_predict = price
+        self.highs.append(high)
+        self.lows.append(low)
+        self.sar.append(sar_predict)
+        return orders
+
+
+print(evaluateHist(SAR(), '^BVSP.csv'))
