3e84ef37453e2ca049bd4a3d1217a2fd709fcd16
raulikeda <rauligs@insper.edu.br>
2019-11-14 07:45:09 -0300

    Update tradingsystem.py

--- tradingsystem.py
+++ tradingsystem.py
@@ -63,7 +63,7 @@ class TradingSystem():
                 order.status = Order.REJECTED
                 if id in self.strategies:
                     strategy = self.strategies[id]
-                    strategy.fill(instrument, 0, 0, order.status)
+                    strategy.fill(order.fill, instrument, 0, 0, order.status)
             else:
                 if order.id not in self.orders:
                     self.orders[order.id] = order
